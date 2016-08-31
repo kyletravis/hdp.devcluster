1b1c18b45edef85c8b0c8aefcd5d193e713cd7db
Kyle Travis <kyle@kyletravis.com>
2016-08-31 05:03:40 +0000

    added pull script

---
+++ scripts/gcloud_pull.sh
@@ -0,0 +1,3 @@
+gcloud docker pull gcr.io/docker-build/ambari_2.2_agent_node && docker tag gcr.io/docker-build/ambari_2.2_agent_node hwxu/ambari_2.2_agent_node
+gcloud docker pull gcr.io/docker-build/ambari_2.2_server_node && docker tag gcr.io/docker-build/ambari_2.2_server_node hwxu/ambari_2.2_server_node
+gcloud docker pull gcr.io/docker-build/ambari_2.2_node && docker tag gcr.io/docker-build/ambari_server_node hwxu/ambari_server_node
