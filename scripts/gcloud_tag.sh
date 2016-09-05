7f5d415c7e8818b2a241ebef51db6530079a294b
Kyle Travis <ktravis@pinsightmedia.com>
2016-09-05 14:33:49 -0500

    enabled expose and added tag script

---
+++ scripts/gcloud_tag.sh
@@ -0,0 +1,3 @@
+docker tag hwxu/ambari_2_agent_node gcr.io/docker-build/ambari_2_agent_node
+docker tag hwxu/ambari_2_server_node gcr.io/docker-build/ambari_2_server_node
+docker tag hwxu/ambari_2_node gcr.io/docker-build/ambari_2_node
